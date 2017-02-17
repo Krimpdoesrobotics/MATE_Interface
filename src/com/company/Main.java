e82717857a846c307bfc72dffad8d002883b6598
Evan <evankrimp@gmail.com>
2017-02-16 22:43:37 -0500

    ---FIXED--- adjs were commented
    ---ADDED---

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,35 +5,34 @@ public class Main
     public static void main(String[] args)
     {
 	// write your code here
-    //FUCK YOU EVAN
 
     }
-    public boolean AdjFL(int power)//adjust the power of the front left motor
+    public boolean AdjFL(int power) //adjust the power of the front left motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
     }
-    public boolean AdjFR(int power)//adjust the power of the front right motor
+    public boolean AdjFR(int power) //adjust the power of the front right motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
     }
-    public boolean AdjBL(int power)//adjust the power of the back left motor
+    public boolean AdjBL(int power) //adjust the power of the back left motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
     }
-    public boolean AdjBR(int power)//adjust the power of the back left motor
+    public boolean AdjBR(int power) //adjust the power of the back right motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
     }
-    public boolean AdjVL(int power)//adjust the power of the vertical left motor
+    public boolean AdjVL(int power) //adjust the power of the vertical left motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
     }
-    public boolean AdjVR(int power)//adjust the power of the vertical left motor
+    public boolean AdjVR(int power) //adjust the power of the vertical right motor
     {
         //TODO implement this method
         return true;//temporary - change when this method is implemented
