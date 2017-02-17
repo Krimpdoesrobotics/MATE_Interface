9ded4a8b8dca510925f7fe5ea4647434b7befb3e
ZacharyOldham <zacharyoldham@gmail.com>
2017-02-16 21:38:09 -0500

    I created a shell for the command methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,41 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
 	// write your code here
     //FUCK YOU EVAN
+
+    }
+    public boolean AdjFL(int power)//adjust the power of the front left motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
+    }
+    public boolean AdjFR(int power)//adjust the power of the front right motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
+    }
+    public boolean AdjBL(int power)//adjust the power of the back left motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
+    }
+    public boolean AdjBR(int power)//adjust the power of the back left motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
+    }
+    public boolean AdjVL(int power)//adjust the power of the vertical left motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
+    }
+    public boolean AdjVR(int power)//adjust the power of the vertical left motor
+    {
+        //TODO implement this method
+        return true;//temporary - change when this method is implemented
     }
 }
