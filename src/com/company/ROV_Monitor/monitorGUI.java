33f7013280c5174afd9d5120a9805272b449d13f
Dival Banerjee <bandiv2011@gmail.com>
2017-03-06 12:40:28 -0500

    Added set background and foreground color methods ﻿

--- src/com/company/ROV_Monitor/monitorGUI.java
+++ src/com/company/ROV_Monitor/monitorGUI.java
@@ -1,9 +1,11 @@
 package com.company.ROV_Monitor;
 
 import com.company.ROV_Monitor.Custom_Components.BackGroundPanel;
+import com.company.ROV_Monitor.Custom_Components.Button;
 import com.company.ROV_Monitor.Custom_Components.GradientPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by bandi on 2/18/2017.
@@ -12,5 +14,13 @@ public class monitorGUI extends JFrame {
     JPanel backGroundPanel = new GradientPanel();
     JLabel lblStatus = new JLabel("Status:");
 
+    Button test = new Button("Test",100,50,
+            80,20,600,20);
 
+
+    public monitorGUI(){
+        setSize(new Dimension(300,300));
+        Container container = getContentPane();
+        container.add(test);
+    }
 }
