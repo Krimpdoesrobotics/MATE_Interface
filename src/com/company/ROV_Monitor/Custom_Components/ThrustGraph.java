552ccb9f914273e5f1127e6aaab3711b02198ddc
Dival Banerjee <bandiv2011@gmail.com>
2017-04-22 16:46:30 -0400

    Parts of the gamepad controller, serial communications and ﻿controller input were updated

--- src/com/company/ROV_Monitor/Custom_Components/ThrustGraph.java
+++ src/com/company/ROV_Monitor/Custom_Components/ThrustGraph.java
@@ -1,9 +1,29 @@
 package com.company.ROV_Monitor.Custom_Components;
 
+import javax.swing.*;
+
 /**
  * Created by bandi on 2/18/2017.
  */
-public class ThrustGraph
-{
+public class ThrustGraph extends JPanel {
+    private int myThurster;
+    private double myPowerPercentage;
+    private static final int SCALE_FACTOR = 1;
+
+    public ThrustGraph(int thrusterNum){
+        myPowerPercentage = 0;
+        myPowerPercentage = 0;
+    }
+    public void updatePercentage(double expectedPower){
+        this.myPowerPercentage = expectedPower;
+        this.repaint();
+    }
+
+    @Override
+    public void repaint(){
+    double graphCenter;
+    }
+
+
 
 }
